efe33367358d57b638200bc6384f1dc159f686dc
stropeala <petre.ispir2002@protonmail.com>
2025-11-13 18:22:38 +0200

    added login and welcome page

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+#Import the required libraries
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__) #Create a Flask app to handle the requests
 
+#Defined the username and password for the login
+USERNAME = "petre.ispir2002@protonmail.com"
+PASSWORD = "PGpBFWDnuUBs@X2Mp%T9"
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/", methods=["GET", "POST"]) #Creating the main route for the login
+def login():
+    if request.method == 'POST': #Authorisation check
+        #get data from
+        username = request.form.get("username")
+        password = request.form.get("password")
 
+        #checking the data
+        if username == USERNAME and password == PASSWORD:
+            return redirect(url_for("Welcome")) #if the credentials are correct direct to welcome page
+        else:
+            return render_template("login.html",
+                                   error="Invalid username or password") #if credentials are incorrect return to login
+    return render_template("login.html")
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/Welcome") #Creating the main route for the welcome page
+def Welcome():
+    return render_template("welcome.html")
+
+#Activating the server
+if __name__ == "__main__":
+    app.run(debug=True)
